f6d30ac6e8c8b99564116527a664795f963a37c6
krufkojat <jakobczyk.michal@gmail.com>
2024-07-26 11:52:42 +0200

    Add auth flow and layout

---
+++ app/(marketing)/blog/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function BlogPage() {
+  return (
+    <main className="mx-4 basis-0.5 px-0 text-center text-2xl">
+      elo melo! blog
+    </main>
+  );
+}
